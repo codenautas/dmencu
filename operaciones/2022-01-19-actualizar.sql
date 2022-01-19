549c7a927d062e5e44e2c679de38b13103848672
GracielaCanessa <gracane@gmail.com>
2022-01-19 15:27:15 -0300

    agrego nuevo tipoc para actualizar en discon (incorporado por Emilio en meta-enc).

--- operaciones/2022-01-19-actualizar.sql
+++ operaciones/2022-01-19-actualizar.sql
@@ -1,2 +1,4 @@
 set search_path=base;
 alter table casilleros add column es_activo boolean default true;
+insert into tipoc(tipoc, denominacion, irrepetible, desp_casillero, desp_hijos, puede_ser_var)
+    values ('FIN', 'fin', true, null, null, false);
