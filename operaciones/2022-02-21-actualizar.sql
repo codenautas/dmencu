775993cd0c1065e8897c108e616030c7cad4663f
Manuel De la Penna <manueldelapenna@gmail.com>
2022-02-24 22:47:34 -0300

    corrijo sql

--- operaciones/2022-02-21-actualizar.sql
+++ operaciones/2022-02-21-actualizar.sql
@@ -55,6 +55,6 @@ UPDATE operativos
         },
 		"cantidad_sorteables":"tp",
 		"cantidad_total":"total_m",
-        "resultado": "cr_num_miembro"
+        "resultado": "cr_num_miembro",
 		"id_formulario_individual":"F:I1"
     }'
