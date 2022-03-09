93c878e370a1c833996df7988939be290267a8e9
GracielaCanessa <gracane@gmail.com>
2022-03-09 17:10:30 -0300

    agregados para la el sorteo automático en la Supervisión

--- operaciones/2022-03-08-actualizar.sql
+++ operaciones/2022-03-08-actualizar.sql
@@ -46,8 +46,53 @@ UPDATE operativos
             "cantidad_sorteables":"tp",
             "cantidad_total":"total_m",
             "resultado": "cr_num_miembro",
+            "resultado_manual":"cr_num_miembro_ing",
             "sorteado_mostrar": [{"source":"nombre", "target":"msnombre"}],
             "id_formulario_individual":"F:I1"
+        },
+        "F:RE_SUP":{
+            "unidad_analisis": "personas_sup",
+            "expr_incompletitud": {
+                "3":{"dominio": 3, "expr": "not(nombre_sup) or not(sexo_sup) or not(edad_sup)"},
+                "5":{"dominio": 5, "expr": "not(nombre_sup) or not(sexo_sup) or not(edad_sup)"}
+            },
+            "disparador": "sorteo",
+            "filtro": {
+                "3":{"dominio": 3, "expr": "edad_sup>=18"},
+                "5":{"dominio": 5, "expr": "edad_sup>=18"}
+            },
+            "orden": [
+                {"variable":"edad_sup" , "orden":-1}
+            ], 
+            "parametros":["nombre_sup","sexo_sup","edad_sup", "sp4_sup", "spp5", "total_m_sup"],
+            "incompletas":"_personas_incompletas_sup",
+            "variableBotonFormularioUA":"$B.F:S1_P_SUP",
+            "metodo": "tabla",
+            "param_metodo": {
+                "var_letra": "l0",
+                "tabla": [
+                    "AAAAAAAAAA",
+                    "BABAABAABB",
+                    "ACCBBABBAC",
+                    "BAACCBDCDA",
+                    "CBEDAEADCB",
+                    "FDBAECEAFD",
+                    "ECDGGFCBBA",
+                    "DGAECDBFHC",
+                    "GEHCBIHDAF",
+                    "AHFBDJGCIE",
+                    "IAGHFEDBIK",
+                    "GDDJAAFECL",
+                    "ACHMEKHJBM",
+                    "JMCHIAENLC",
+                    "OGCKMIKMJN"
+                ]
+            },
+            "cantidad_sorteables":"total_rango_sup",
+            "cantidad_total":"total_m_sup",
+            "resultado": "nro_miembro_sel_sup",
+            "resultado_manual":"nro_mie_sel_ing_sup",
+            "sorteado_mostrar": [{"source":"nombre_sup", "target":"msnombre"}],
         }
     }'
 where operativo = 'PREJU_2022';
