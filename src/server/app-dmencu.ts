67734004a133a02c070192a9d0ed0d33fb4bb90e
Manuel De la Penna <manueldelapenna@gmail.com>
2022-03-15 16:22:43 -0300

    subimos version de app y cambiamos color

--- src/server/app-dmencu.ts
+++ src/server/app-dmencu.ts
@@ -49,7 +49,7 @@ import { control_resumen     } from './table-control_resumen';
 
 import {defConfig} from "./def-config"
 
-const APP_DM_VERSION="#22-03-15";
+const APP_DM_VERSION="#22-03-15b";
 
 export type Constructor<T> = new(...args: any[]) => T;
 export function emergeAppDmEncu<T extends Constructor<procesamiento.AppProcesamientoType>>(Base:T){
@@ -168,7 +168,7 @@ export function emergeAppDmEncu<T extends Constructor<procesamiento.AppProcesami
                   "start_url": "../campo",
                   "display": "standalone",
                   "theme_color": "#3F51B5",
-                  "background_color": "#FED214"
+                  "background_color": "#6d60ed"
                 }
                 miniTools.serveText(JSON.stringify(content), 'application/json')(req,res);
             }catch(err){
