529bfbdf7b84bd8a59cdaf213287e590ec3de415
Manuel De la Penna <manueldelapenna@gmail.com>
2022-03-18 16:18:15 -0300

    subo version app dm

--- src/server/app-dmencu.ts
+++ src/server/app-dmencu.ts
@@ -49,7 +49,7 @@ import { control_resumen     } from './table-control_resumen';
 
 import {defConfig} from "./def-config"
 
-const APP_DM_VERSION="#22-03-16";
+const APP_DM_VERSION="#22-03-18";
 
 export type Constructor<T> = new(...args: any[]) => T;
 export function emergeAppDmEncu<T extends Constructor<procesamiento.AppProcesamientoType>>(Base:T){
