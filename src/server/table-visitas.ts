26c3030c568f2e3fca7ddb38ac5c37afc0eff20b
Raquel Salva <raquelzssalva@gmail.com>
2022-04-27 12:24:26 -0300

    Modificar el tipo de datos de fecha, hora en los archivos ts correspondientes a visitas y visitas_sup

--- src/server/table-visitas.ts
+++ src/server/table-visitas.ts
@@ -44,12 +44,12 @@ export function visitas(context:TableContext):TableDefinition {
         },
         {
             "name": "fecha",
-            "typeName": "date",
+            "typeName": "text",
             "nullable": true
         },
         {
             "name": "hora",
-            "typeName": "interval",
+            "typeName": "text",
             "nullable": true
         },
         {
--- src/server/table-visitas_sup.ts
+++ src/server/table-visitas_sup.ts
@@ -44,12 +44,12 @@ export function visitas_sup(context:TableContext):TableDefinition {
         },
         {
             "name": "fecha_sup",
-            "typeName": "date",
+            "typeName": "text",
             "nullable": true
         },
         {
             "name": "hora_sup",
-            "typeName": "interval",
+            "typeName": "text",
             "nullable": true
         },
         {
