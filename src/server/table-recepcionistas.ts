03c2f751a40398229575cc5651ac770f8e58a869
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2022-07-16 21:10:33 -0300

    agrega tipos

--- src/server/table-recepcionistas.ts
+++ src/server/table-recepcionistas.ts
@@ -1,6 +1,7 @@
 "use strict";
 
 import {personal} from "./table-personal"
+import { TableContext, TableDefinition } from "./types-dmencu";
 
 export function recepcionistas(context:TableContext):TableDefinition {
     return personal(context, {rol:'recepcionista', name:'recepcionistas'})    
