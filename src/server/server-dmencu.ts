3871736e4649657fa48bb49ac856f78f653b585c
GracielaCanessa <gracane@gmail.com>
2022-10-04 18:32:04 -0300

    agregando funciones en dmencu y subimos la versión

--- src/server/server-dmencu.ts
+++ src/server/server-dmencu.ts
@@ -1,13 +1,13 @@
 "use strict";
 
 import { AppBackend, emergeAppMetaEnc, emergeAppOperativos, emergeAppRelEnc } from "meta-enc";
-import { emergeAppConsistencias, emergeAppDatosExt, emergeAppProcesamiento, emergeAppVarCal, OperativoGenerator /*, pgWitheList */} from "procesamiento";
+import { emergeAppConsistencias, emergeAppDatosExt, emergeAppProcesamiento, emergeAppVarCal, OperativoGenerator , pgWhiteList } from "procesamiento";
 import { emergeAppDmEncu } from "./app-dmencu";
 
 OperativoGenerator.mainTD = 'viviendas';
 OperativoGenerator.mainTDPK = 'vivienda'; // TODO: hacer esto dinámico en paquete consistencias
 
-//pgWitheList.push('abs');
+pgWhiteList.push('blanco');
 
 var AppDmEncu = emergeAppDmEncu(
     emergeAppProcesamiento(
@@ -18,7 +18,6 @@ var AppDmEncu = emergeAppDmEncu(
                         emergeAppVarCal(
                             emergeAppOperativos(AppBackend)
                         )
-                    )
                 )
             )
         )
