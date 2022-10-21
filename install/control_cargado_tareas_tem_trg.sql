8fa2b639663b4b69c7d1129a1796a6e425ceb2d8
Raquel Salva <raquelzssalva@gmail.com>
2022-10-21 12:27:50 -0300

    Agrego control por trigger que no se pueda poner cargar en mas de una tarea

--- install/control_cargado_tareas_tem_trg.sql
+++ install/control_cargado_tareas_tem_trg.sql
@@ -6,22 +6,30 @@ CREATE OR REPLACE FUNCTION control_cargado_tareas_tem_trg()
     LANGUAGE 'plpgsql'
 AS $BODY$
 declare 
-  cant_cargados integer;
-  tarea_cargada text;
+    cant_cargado  integer;
+    tarea_cargado text;
+    cant_cargar   integer;
+    tarea_cargar  text;
 begin
     if new.operacion='cargar'  then
-        select count(*), min(tarea) 
-            into cant_cargados, tarea_cargada
+        select count(*)filter (where tt.cargado_dm is not null), min(tarea) filter (where tt.cargado_dm is not null)
+             , count(*)filter (where tt.operacion='cargar'), min(tarea) filter (where tt.operacion='cargar')
+            into cant_cargado, tarea_cargado,cant_cargar,tarea_cargar
             from tareas_tem tt join parametros using(operativo)
-            where tt.enc=old.enc and tt.cargado_dm is not null and tt.tarea is distinct from new.tarea
+            where tt.enc=old.enc and tt.tarea is distinct from new.tarea
                 and unico_registro;
-        if cant_cargados >0 then
-            raise exception 'Error: la encuesta % ya esta cargada para la tarea %', old.enc, tarea_cargada;
+        if cant_cargado >0 then
+            raise exception 'Error: la encuesta % ya esta cargada en dm para la tarea %', old.enc, tarea_cargada;
         end if;
+        if cant_cargar >0 then
+            raise exception 'Error: la encuesta % ya tiene operacion cargar para la tarea %', old.enc, tarea_cargar;
+        end if;
+        
     end if;    
     return new;
 end;
 $BODY$;
+
 
 CREATE TRIGGER control_cargado_tareas_tem_trg
     BEFORE UPDATE OF operacion
