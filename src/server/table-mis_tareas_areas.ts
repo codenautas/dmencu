7bef8d9a3490b00a112860f8ecfcb92a5108f8b1
manueldelapenna <manueldelapenna@gmail.com>
2023-03-09 16:08:29 -0300

    saco heredadas de tareas_areas de dump

--- src/server/table-mis_tareas_areas.ts
+++ src/server/table-mis_tareas_areas.ts
@@ -5,6 +5,8 @@ import {TableDefinition, TableContext} from "./types-dmencu";
 import {tareas_areas} from "./table-tareas_areas";
 
 export function mis_tareas_areas(context:TableContext):TableDefinition {
-    return tareas_areas(context, {mis:true});
+    var tableDef = tareas_areas(context, {mis:true});
+    tableDef.sql!.isTable = false;
+    return tableDef
 }
 
