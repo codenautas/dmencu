04fd44fee8773453a1b19b93d28d146d583e98bc
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2023-04-05 21:56:55 -0300

    version postgres

--- install/automuestra_trigger.sql
+++ install/automuestra_trigger.sql
@@ -1,32 +1,17 @@
-
-CREATE TRIGGER insert_tem_trigger
-AFTER UPDATE ON areas
-FOR EACH ROW
-WHEN (NEW.cantidad_encuestas <= (SELECT maximo_creacion_encuestas FROM parametros LIMIT 1))
+CREATE OR REPLACE FUNCTION crear_encuestas() RETURNS trigger AS $$
+DECLARE
+    n INTEGER;
+    max_enc INTEGER;
 BEGIN
-  DECLARE enc VARCHAR(50);
-  DECLARE maximo_creacion_encuestas INT;
-
-  SELECT maximo_creacion_encuestas INTO maximo_creacion_encuestas FROM parametros LIMIT 1;
-
-  SET enc = CONCAT(LPAD(NEW.area, LENGTH(NEW.cantidad_encuestas), '0'), '_');
-
-  IF (NEW.cantidad_encuestas > OLD.cantidad_encuestas) THEN
-    DECLARE i INT DEFAULT OLD.cantidad_encuestas + 1;
-    DECLARE insert_count INT DEFAULT 0;
-
-    WHILE (i <= NEW.cantidad_encuestas AND insert_count < maximo_creacion_encuestas) DO
-      SET enc = CONCAT(enc, LPAD(i, LENGTH(NEW.cantidad_encuestas), '0'));
-
-      INSERT INTO tem (enc) VALUES (enc);
-
-      SET i = i + 1;
-      SET insert_count = insert_count + 1;
-    END WHILE;
-  ELSE
-    SET enc = CONCAT(enc, LPAD(NEW.cantidad_encuestas, LENGTH(NEW.cantidad_encuestas), '0'));
-
-    INSERT INTO tem (enc) VALUES (enc);
-  END IF;
+    IF NEW.cantidad_encuestas > OLD.cantidad_encuestas THEN
+        SELECT min(maximo_creacion_encuestas, NEW.cantidad_encuestas)-coalesce(OLD.cantidad_encuestas,0) INTO n FROM parametros;
+        FOR j IN 1..(n) LOOP
+            INSERT INTO tem (enc) VALUES (LPAD((NEW.area::text), length(NEW.cantidad_encuestas), '0'));
+        END LOOP;
+    END IF;
+    RETURN NULL;
 END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER areas_encuestas_trigger AFTER UPDATE OF cantidad_encuestas ON areas
+FOR EACH ROW EXECUTE FUNCTION crear_encuestas();
