4af70ef18faff6c59dc176c727ed0ab28d29f4eb
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2023-04-10 15:51:19 -0300

    versión funcional setea varios campos

--- install/automuestra_trigger.sql
+++ install/automuestra_trigger.sql
@@ -1,17 +1,24 @@
-CREATE OR REPLACE FUNCTION crear_encuestas() RETURNS trigger AS $$
+set search_path=base;
+
+CREATE OR REPLACE FUNCTION crear_encuestas() RETURNS trigger LANGUAGE plpgsql 
+AS $$
 DECLARE
     n INTEGER;
     max_enc INTEGER;
 BEGIN
-    IF NEW.cantidad_encuestas > OLD.cantidad_encuestas THEN
-        SELECT min(maximo_creacion_encuestas, NEW.cantidad_encuestas)-coalesce(OLD.cantidad_encuestas,0) INTO n FROM parametros;
+    IF NEW.cantidad_encuestas > coalesce(OLD.cantidad_encuestas,0) THEN
+            SELECT coalesce(max(enc::bigint), NEW.area) INTO max_enc FROM tem WHERE area = NEW.area;
+        SELECT least(maximo_creacion_encuestas, NEW.cantidad_encuestas)-coalesce(OLD.cantidad_encuestas,0) INTO n FROM parametros;
         FOR j IN 1..(n) LOOP
-            INSERT INTO tem (enc) VALUES (LPAD((NEW.area::text), length(NEW.cantidad_encuestas), '0'));
+            INSERT INTO tem (operativo,     enc,               estado_actual,  habilitada, area) 
+                     VALUES (NEW.operativo, (max_enc+j)::text, 'ESTADO_NUEVO', true,       NEW.area);
         END LOOP;
     END IF;
     RETURN NULL;
 END;
-$$ LANGUAGE plpgsql;
+$$ ;
 
-CREATE TRIGGER areas_encuestas_trigger AFTER UPDATE OF cantidad_encuestas ON areas
-FOR EACH ROW EXECUTE FUNCTION crear_encuestas();
+DROP TRIGGER IF EXISTS areas_encuestas_trigger ON areas;
+CREATE TRIGGER areas_encuestas_trigger 
+    AFTER UPDATE OF cantidad_encuestas ON areas
+    FOR EACH ROW EXECUTE FUNCTION crear_encuestas();
