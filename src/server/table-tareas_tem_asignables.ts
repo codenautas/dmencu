76836e667f7845b9f12a5f733380e53e5162e01b
Emilio Platzer <emilioplatzer@gmail.com>
2023-04-16 15:50:30 -0300

    hago funcionar tareas_tem_asignables

--- src/server/table-tareas_tem_asignables.ts
+++ src/server/table-tareas_tem_asignables.ts
@@ -1,45 +1,53 @@
 "use strict";
 
-import {TableDefinition, TableContext, FieldDefinition} from "./types-dmencu";
+import {TableDefinition, TableContext, FieldDefinition, OtherTableDefs} from "./types-dmencu";
 
-import {tareas_tem} from "./table-tareas_tem";
+import { tem } from "./table-tem";
 import { setCommonDefinition } from "./table-tareas_tem_recepcion";
 
 export function tareas_tem_asignables(context:TableContext):TableDefinition {
-    //var tareas = ['encu','recu','supe'];
-    //var extraFields:FieldDefinition[] = [];
-    //var extraFrom = '';
-    //var extraSelect = '';
-    //var otherTables = {}
+    var tareas = ['encu','recu','supe'];
+    var tareas_fields = [
+        {name:'recepcionista_tarea', prefijo:'rec'},
+        {name:'asignado', prefijo:'per'},
+        {name:'estado', prefijo:'est'}
+    ]
+    var extraFields:FieldDefinition[] = [];
+    var extraFrom = '';
+    var extraSelect = '\n';
+    var otherTableDefs:OtherTableDefs = {}
 
-    //tareas.forEach(t=>{
-    //    extraFields.push({name:`rec_${t}`, typeName:'text', table:`tem_${t}`});
-    //    extraFields.push({name:`per_${t}`, typeName:'text', table:`tem_${t}`});
-    //    extraFields.push({name:`est_${t}`, typeName:'text', table:`tem_${t}`});
-    //    extraSelect += `, asignante as rec_${t}, tem_${t}.asignado as per_${t}, tem_${t}.estado as est_${t}`;
-    //    extraFrom += ` left join tareas_tem as tem_${t} on tem_${t}.operativo = tt.operativo and tem_${t}.enc = tt.enc and tem_${t}.tarea = '${t}'`; 
-    //    otherTables[`tem_${t}`] = {
-    //        //name: 'tareas_tem', 
-    //        //tableName:'tareas_tem', 
-    //        prefilledField:{tarea:t}
-    //    }
-    //});
+    tareas.forEach(t=>{
+        tareas_fields.forEach(f=>{
+            extraFields.push({name:`${f.prefijo}_${t}`, typeName:'text', table:`tem_${t}`, nameForUpsert:f.name});
+            extraSelect += `, tem_${t}.${f.name} as ${f.prefijo}_${t}`;
+        })
+        extraFrom += ` 
+            left join tareas_tem as tem_${t} on tem_${t}.operativo = t.operativo and tem_${t}.enc = t.enc and tem_${t}.tarea = '${t}'`; 
+        otherTableDefs[`tem_${t}`] = {
+            name: 'tareas_tem', 
+            tableName:'tareas_tem', 
+            prefilledField:{tarea:t}
+        }
+    });
    
-    var tableDef = tareas_tem(context);
+    var tableDef = tem(context, {});
     tableDef.name = `tareas_tem_asignables`;
-    setCommonDefinition(tableDef);
-    //tableDef.primaryKey = ['operativo','enc'];
-    //tableDef.fields = tableDef.fields.filter((field)=>
-    //    ['operativo','enc','tarea','estado', 'area', 'asignado', 'asignante', 'abrir','consistir','tarea_anterior','cargado', 'cargado_dm','operacion']
-    //    .includes(field.name)
-    //).concat(extraFields);
-    //tableDef.softForeignKeys=[
-    //    {references:'tokens', fields:[{source:'cargado_dm', target:'token'}], displayFields:['username'], displayAfterFieldName:'cargado'},
-    //],
+    // setCommonDefinition(tableDef);
+    tableDef.primaryKey = ['operativo','enc'];
+    tableDef.fields = tableDef.fields.filter((field)=>
+        ['operativo','enc','tarea_actual', 'estado_actual', 'habilitada', 'area', 'tarea_proxima', 'cargado_dm']
+        .includes(field.name)
+    ).concat(extraFields);
+    /*
+    tableDef.softForeignKeys=[
+        {references:'tokens', fields:[{source:'cargado_dm', target:'token'}], displayFields:['username'], displayAfterFieldName:'cargado'},
+    ],
     tableDef.hiddenColumns=['cargado_dm','notas'];
-    //tableDef.sql!.from =  `(select tt.* ${extraSelect} from (${tableDef.sql!.from}) tt  ${extraFrom})`,
-    //tableDef.sql!.otherTableDefs = otherTables
-    //tableDef.sql!.where = `(tarea_actual="tareas_tem".tarea or tarea_actual is null)`;
+    */
+    tableDef.sql!.from =  `(select t.* ${extraSelect} from (${tableDef.sql!.from}) t ${extraFrom})`,
+    tableDef.sql!.otherTableDefs = otherTableDefs
+    // tableDef.sql!.where = `(tarea_actual="tareas_tem".tarea or tarea_actual is null)`;
     return tableDef
 }
 
