af28d16ff3dd983f6df316b4cb48852241204a01
manueldelapenna <manueldelapenna@gmail.com>
2024-03-12 17:25:10 -0300

    agrego funcion faltante

--- install/esquema_comun.sql
+++ install/esquema_comun.sql
@@ -58,3 +58,27 @@ CREATE OR REPLACE FUNCTION comun.es_par(p_valor anyelement)
     AS $BODY$select case when mod(p_valor,2)=0 then TRUE else FALSE end$BODY$
     LANGUAGE 'sql'
     IMMUTABLE ;
+
+CREATE OR REPLACE FUNCTION comun.cadena_normalizar(
+    p_cadena text)
+    RETURNS text
+    LANGUAGE 'sql'
+    IMMUTABLE 
+AS $BODY$
+/*
+-- Pruebas:
+select entrada, esperado, comun.cadena_normalizar(entrada)
+    , esperado is distinct from comun.cadena_normalizar(entrada)
+  from (
+  select 'hola' as entrada, 'HOLA' as esperado
+  union select 'Cañuelas', 'CAÑUELAS'
+  union select 'ÁCÉNTÍTÓSÚCü','ACENTITOSUCU'
+  union select 'CON.SIGNOS/DE-PUNTUACION    Y MUCHOS ESPACIOS','CON SIGNOS DE-PUNTUACION Y MUCHOS ESPACIOS'
+  union select 'CONÁÀÃÄÂáàãäâ   A', 'CONAAAAAAAAAA A'
+  union select 'vocalesÁÒöÈÉüÙAeùúÍî?j', 'VOCALESAOOEEUUAEUUII J'
+  union select 'ÅåÕõ.e', 'AAOO E'
+) casos
+  where esperado is distinct from comun.cadena_normalizar(entrada);
+*/
+  select upper(trim(regexp_replace(translate ($1, 'ÁÀÃÄÂÅÉÈËÊÍÌÏÎÓÒÖÔÕÚÙÜÛáàãäâåéèëêíìïîóòöôõúùüûçÇ¿¡!:;,?¿"./,()_^[]*$', 'AAAAAAEEEEIIIIOOOOOUUUUaaaaaaeeeeiiiiooooouuuu                      '), ' {2,}',' ','g')));
+$BODY$;
