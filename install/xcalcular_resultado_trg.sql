8d7481a327346caae54859164be340445502740c
Raquel Salva <raquelzssalva@gmail.com>
2025-12-02 17:10:43 -0300

    calculo de resultado, tomo operativo de la tabla parametros

--- install/xcalcular_resultado_trg.sql
+++ install/xcalcular_resultado_trg.sql
@@ -7,9 +7,21 @@ AS $BODY$
 declare
   vresultado       text;
 begin
-    select  case when new.rea=1 then 'efectiva'  when new.rea=3 then 'pendiente' when new.rea=4 then 'mixta' when (new.rea=2 and new.norea is not null) then (select grupo from no_rea where new.norea=no_rea::integer )  else null end  into vresultado
-      from tem
-      where operativo='GGS_2022' and enc=new.enc;
+    select  
+        case 
+          when new.rea=1 then 'efectiva'  
+          when new.rea=3 then 'pendiente' 
+          when new.rea=4 then 'mixta' 
+          when (new.rea=2 and new.norea is not null) then
+            (select grupo 
+              from no_rea 
+              where new.norea=no_rea::integer
+            )  
+          else null 
+        end  into vresultado
+      from tem t join parametros p using (operativo)
+      where p.unico_registro
+        and enc=new.enc;
     
     new.resultado= vresultado ;
     return new; 
